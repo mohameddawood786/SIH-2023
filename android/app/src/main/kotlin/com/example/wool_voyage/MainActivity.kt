aacd244cf1528c884bb4fdf6f90b282253a09a1a
Mohamed Dawood <mhddawood786@gmail.com>
2023-12-13 19:45:25 +0530

    Main Code

---
+++ android/app/src/main/kotlin/com/example/wool_voyage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wool_voyage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/wool_warehousing.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/material.dart';
+// import 'package:wool_voyage/search_bale.dart';
+
+// void main() {
+//   runApp(MaterialApp(
+//     home: Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Storage'),
+//         backgroundColor: const Color(0xFFe9dbff),
+//       ),
+//       body: const SearchBale(),
+//       backgroundColor: const Color.fromARGB(255, 252, 252, 252),
+//     ), 
+//   ));
+// }
